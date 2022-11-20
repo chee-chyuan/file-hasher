76381800193a9e275743dd83c2c63c8652b031dd
Cuong <tccuong1999@gmail.com>
2022-11-21 00:16:22 +0800

    modify filehashercommitment.sol

--- contract/src/FileHasherCommitment.sol
+++ contract/src/FileHasherCommitment.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.17;
 
-import "@openzeppelin/access/Ownable.sol";
+// import "@openzeppelin/access/Ownable.sol";
+import "@openzeppelin/access/AccessControl.sol";
 
-contract FileHasherCommitment is Ownable {
+contract FileHasherCommitment is AccessControl {
     uint8 public constant RING_BUFFER_SIZE = 5;
     /// maps address of user to ring buffer
     mapping(address => uint256[RING_BUFFER_SIZE]) public fileHashRingBuffers;
@@ -12,7 +13,11 @@ contract FileHasherCommitment is Ownable {
 
     event CommittedFile(address indexed user, uint256 hash);
 
-    function commitFileHash(address user, uint256 hash) external onlyOwner {
+    constructor () {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function commitFileHash(address user, uint256 hash) external onlyRole(DEFAULT_ADMIN_ROLE) {
         uint8 currentIndex = ringBufferIndexes[user];
         ringBufferIndexes[user] = (currentIndex + 1) % RING_BUFFER_SIZE;
 
