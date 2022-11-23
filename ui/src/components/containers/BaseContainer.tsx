f67f315f52b3691f4b45877a5631bf5aaa07b201
Chen Wen Kang <23054115+cwkang1998@users.noreply.github.com>
2022-11-24 01:25:31 +0800

    feat(ui): complete page 1 layout (#19)

--- ui/src/components/containers/BaseContainer.tsx
+++ ui/src/components/containers/BaseContainer.tsx
@@ -1,19 +1,11 @@
-import { Flex, useColorMode, FlexProps } from "@chakra-ui/react";
+import { Flex, FlexProps } from "@chakra-ui/react";
 
 const BaseContainer = (props: FlexProps) => {
-  const { colorMode } = useColorMode();
-
-  const bgColor = { light: "gray.50", dark: "gray.900" };
-
-  const color = { light: "black", dark: "white" };
-
   return (
     <Flex
       direction="column"
       alignItems="center"
       justifyContent="flex-start"
-      bg={bgColor[colorMode]}
-      color={color[colorMode]}
       {...props}
     />
   );
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,14 +1,16 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { theme } from "./styles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>
 );
