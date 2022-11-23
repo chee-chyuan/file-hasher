b63ea0846fc32385dd2704756b6b87e9c559402f
Sheng Yong Kim <kimshengyong@gmail.com>
2022-11-24 02:50:58 +0800

    update verify code, fix issues

--- ui/src/components/VerifyPage.tsx
+++ ui/src/components/VerifyPage.tsx
@@ -6,81 +6,50 @@ import { ethers } from "ethers";
 import { Card, CardHeader, CardBody, CardFooter, Heading, Stack, StackDivider, Box, Text, Button, Center } from '@chakra-ui/react'
 import { Remote } from "comlink";
 import { FileHasher } from "../file-hasher.worker";
+import { useAccount } from 'wagmi'
+import fileHashContractDetails from "../artifacts/contracts/FileHash.sol/FileHash.json"
 
 export type VerifyProps = {
-    wasmWorkerApi: Remote<FileHasher>;
-  };
+  wasmWorkerApi: Remote<FileHasher>;
+};
 
-export default function Verify({ wasmWorkerApi} : VerifyProps) {
+export type JsonFileContentType = {
+  selectedRow: string;
+  selectedContent: string;
+  proof: any
+}
+
+export default function Verify({ wasmWorkerApi }: VerifyProps) {
+  const { address, isConnected } = useAccount()
   const styles = useMultiStyleConfig("Button", { variant: "outline" });
-  const [jsonFileContent, setJsonFileContent] = React.useState<any>();
+  const [jsonFileContent, setJsonFileContent] = React.useState<JsonFileContentType>();
 
-  const verification = async function (e: any) {
+  const uploadFile = async function (e: any) {
     const reader = new FileReader();
     reader.onload = async (e: any) => {
       console.log(e)
       const result = e?.target?.result;
       const jsonResult = JSON.parse(result);
       setJsonFileContent(jsonResult)
-      // setJsonFileContent({})
-      // console.log(JSON.parse(result));
-      const abi = `
-      [{
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "name": "fileHashRingBuffers",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "ringBufferIndexes",
-        "outputs": [
-          {
-            "internalType": "uint8",
-            "name": "",
-            "type": "uint8"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      }]`
-    // console.log("Contract address is ",process.env.NEXT_PUBLIC_CONTRACT_ADDRESS);
-    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!, abi, new ethers.providers.AlchemyProvider("goerli",process.env.NEXT_PUBLIC_ALCHEMY_API_KEY))
-    const currIndex = await contract.ringBufferIndexes(jsonResult.address)
-    const commitmentHash = await contract.fileHashRingBuffers(currIndex);
-    const verifyResult = await wasmWorkerApi.verifyProof(jsonFileContent.proof, jsonFileContent.selectedRow, jsonFileContent.selectedContent, commitmentHash);
-    alert(verifyResult);
-    // console.log(contract)
-    // await getFileCommitment(["1","2"], ["3","4"])
     }
-    // console.log(data);
     reader.readAsText(e.target?.files[0]);
   }
-
+  const verifyProof = async (wasmWorkerApi: Remote<FileHasher>, address: string, jsonFileContet: JsonFileContentType) => {
+    console.log("Contract address is ", process.env.REACT_APP_PUBLIC_CONTRACT_ADDRESS);
+    console.log("REACT_APP_PUBLIC_ALCHEMY_API_KEY is ", process.env.REACT_APP_ALCHEMY_API_KEY);
+    const contract = new ethers.Contract(process.env.REACT_APP_PUBLIC_CONTRACT_ADDRESS!, fileHashContractDetails.abi, new ethers.providers.AlchemyProvider("goerli", process.env.REACT_APP_ALCHEMY_API_KEY))
+    const currIndex = await contract.ringBufferIndexes(address)
+    const commitmentHash = await contract.fileHashRingBuffers(address, currIndex);
+    const verifyResult = await wasmWorkerApi.verifyProof(jsonFileContet.proof, jsonFileContet.selectedRow, jsonFileContet.selectedContent, commitmentHash);
+    alert(verifyResult);
+    console.log("verifyResults: ", verifyResult);
+    // return { verifyResult }
+  }
+  if (!isConnected) return (
+    <Center>
+      <Text>Please Connect wallet to proceed</Text>
+    </Center>
+  )
   return (
     <>
       <Input
@@ -96,67 +65,76 @@ export default function Verify({ wasmWorkerApi} : VerifyProps) {
             ...styles,
           },
         }}
-        onChange={verification}
+        disabled={!isConnected}
+        onChange={uploadFile}
       />
 
-      {jsonFileContent ? 
+      {jsonFileContent ?
         (<>
-        <Card>
-        <CardHeader>
-          <Heading size='md'>Proof</Heading>
-        </CardHeader>      
-        <CardBody>
-          <Stack divider={<StackDivider />} spacing='4'>
-            <Box>
-              <Heading size='xs' textTransform='uppercase'>
-                Smart contract address
-              </Heading>
-              <Text pt='2' fontSize='sm'>
-                {process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}
-              </Text>
-            </Box>
-            <Box>
-              <Heading size='xs' textTransform='uppercase'>
-                Verifier Address
-              </Heading>
-              <Text pt='2' fontSize='sm'>
-                {jsonFileContent.address}
-              </Text>
-            </Box>
-            <Box>
-              <Heading size='xs' textTransform='uppercase'>
-                Selected Row 
-              </Heading>
-              <Text pt='2' fontSize='sm'>
-                {jsonFileContent.selectedRow}
-              </Text>
-            </Box>
-            <Box>
-              <Heading size='xs' textTransform='uppercase'>
-                Selected Content 
-              </Heading>
-              <Text pt='2' fontSize='sm'>
-                {jsonFileContent.selectedContent}
-              </Text>
-            </Box>
-            <Box>
-              <Heading size='xs' textTransform='uppercase'>
-                Proof
-              </Heading>
-              <Text pt='2' fontSize='sm'>
-                {jsonFileContent.proof}
-              </Text>
-            </Box>            
-          </Stack>
-        </CardBody>
-      </Card>
-      <Center>
-        <Button colorScheme='blue' size='lg'>
-        Verify
-        </Button>
-      </Center>
-     </>
-      ) : <></>
+          <Box mt="2">
+            <Card>
+              <CardHeader>
+                <Heading size='md'>Proof</Heading>
+              </CardHeader>
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing='4'>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Smart contract address
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      {process.env.REACT_APP_PUBLIC_CONTRACT_ADDRESS}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Verifier Address
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      {address}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Selected Row
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      {jsonFileContent.selectedRow}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Selected Content
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      {jsonFileContent.selectedContent}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='xs' textTransform='uppercase'>
+                      Proof
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      {jsonFileContent.proof}
+                    </Text>
+                  </Box>
+                </Stack>
+              </CardBody>
+            </Card>
+          </Box>
+          <Box mt="2">
+            {
+              (isConnected && address) && (
+                <Center>
+                  <Button colorScheme='blue' size='lg' onClick={() => verifyProof(wasmWorkerApi, address, jsonFileContent)}>
+                    Verify Proof
+                  </Button>
+                </Center>
+              )
+            }
+          </Box>
+        </>
+        ) : <></>
       }
     </>
   );
