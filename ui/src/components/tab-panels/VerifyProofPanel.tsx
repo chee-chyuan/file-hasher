5222b09604eb8dccd338144f1f96daeee850d6cd
chen <23054115+cwkang1998@users.noreply.github.com>
2022-11-23 15:51:40 +0800

    feat(ui): add row select table

---
+++ ui/src/components/tab-panels/VerifyProofPanel.tsx
@@ -0,0 +1,7 @@
+export const VerifyProofPanel = () => {
+  return (
+    <>
+      <p>three!</p>
+    </>
+  );
+};
---
+++ ui/src/components/tables/RowSelectTable.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  RadioGroup,
+  Radio,
+} from "@chakra-ui/react";
+
+export type FlexibleFormTableProps = {
+  rowTitles?: string[];
+  rowValues?: string[];
+  onChange?: (nextVal: string) => any;
+};
+
+export const RowSelectTable = ({
+  rowTitles = [],
+  rowValues = [],
+  onChange = () => {},
+}: FlexibleFormTableProps) => {
+  const [selectedRow, setSelectedRow] = useState(rowTitles[0]);
+
+  const onRowSelected = (nextVal: string) => {
+    setSelectedRow(nextVal);
+    onChange(nextVal);
+  };
+
+  return (
+    <TableContainer>
+      <form>
+        <RadioGroup value={selectedRow} onChange={onRowSelected}>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Keys</Th>
+                <Th>Value</Th>
+                <Th>Selection</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {rowTitles.map((row, idx) => (
+                <Tr key={idx}>
+                  <Td>{row}</Td>
+                  <Td>{rowValues[idx]}</Td>
+                  <Td>
+                    <Radio value={row} />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </RadioGroup>
+      </form>
+    </TableContainer>
+  );
+};
